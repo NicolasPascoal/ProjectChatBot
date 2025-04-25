cab3deac85fb8072e28b093dc0ca0f5a57c51cd2
Nicolas <nicolasgomesp2006@gmail.com>
2025-04-25 00:39:57 -0300

    atualização, campo de jogadores no bot

--- bot.py
+++ bot.py
@@ -5,7 +5,11 @@ TOKEN = "7709876744:AAG_jh6IEuZm5MeRqYPt8raXYUtnrrO4F_0"
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Salve, FURIOSO(A)! 🔥 Bem-vindo ao bot da FURIA!")
 
+async def jogadores(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("TIME DE VALORANT🔥: \n Pryze, Khalil, Heat, Havoc\n TIME DE CS2🔥: \n FalleN, Kscerato, Yuurih, Skullz, Chelo")
+
 if __name__ == '__main__':
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("jogadores", jogadores))
     app.run_polling()
